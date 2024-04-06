b98a64f625220c15483a77b786041f1a13afee42
Μιχαίας <mmcadoulos@gmail.com>
2024-04-06 12:47:45 -0500

    finished hashmap exercsise

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/HashMapPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/HashMapPractice.java
@@ -1,7 +1,33 @@
 package org.launchcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class HashMapPractice {
     public static void main(String[] args) {
+        HashMap<Integer, String> studentInfo = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String studentName;
+        Integer studentID;
+        System.out.println("Please enter student's info (ENTER to finish): ");
+        do {
+            System.out.println("Student's name: ");
+            studentName = input.nextLine();
+            // add code for the ID info
+            if(!studentName.isEmpty()){
+                System.out.println("Student's ID number: ");
+                studentID = input.nextInt();
 
+                studentInfo.put(studentID, studentName);
+                input.nextLine();
+            }
+
+        }while(!studentName.isEmpty());
+
+        for (Map.Entry<Integer, String> student : studentInfo.entrySet()){
+            System.out.println(student.getValue() + "'s student ID is: " + student.getKey());
+
+        }
     }
 }
